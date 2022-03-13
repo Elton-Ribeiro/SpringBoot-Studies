282b7e4b82278095828f99510477824f7e4c5f6e
Elton-Ribeiro <ear.elton@gmail.com>
2022-03-13 17:10:48 -0300

    RestController, Request Mapping & Request Body

--- Example-2/src/main/java/com/example/demo/DemoController.java
+++ Example-2/src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+	private static String SENTENCE = "This server is running with two potatoes!";
+
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloWorld() {
-		return "This server is running with two potatoes!";
+		return SENTENCE;
+	}
+
+	@RequestMapping(value = "/update/sentence", method = RequestMethod.POST) 
+	public void updateSentence(@RequestBody String updatedSentence) { 
+		SENTENCE = updatedSentence;
 	}
 }
