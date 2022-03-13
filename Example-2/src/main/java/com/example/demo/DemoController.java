11d93b67900c1c16d84ff222a42c95f775b14ba2
Elton-Ribeiro <ear.elton@gmail.com>
2022-03-13 18:12:29 -0300

    RestController, Value, RequestMapping & RequestBody

--- Example-2/src/main/java/com/example/demo/DemoController.java
+++ Example-2/src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,12 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
 	private static String SENTENCE = "This server is running with two potatoes!";
+	private final String propertySentence;
+	
+	@Autowired
+	public DemoController(@Value("${demo.sentence}") String propertySentence) {
+		this.propertySentence = propertySentence; 
+	}
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloWorld() {
 		return SENTENCE;
 	}
 
+	@RequestMapping(value = "/property/sentence", method = RequestMethod.GET)
+	public String propertySentence() {
+		return propertySentence;
+	}
+	
 	@RequestMapping(value = "/update/sentence", method = RequestMethod.POST) 
 	public void updateSentence(@RequestBody String updatedSentence) { 
 		SENTENCE = updatedSentence;
